14ad7ea34b6f7df25bfd6394d8c915a7d2554953
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-10-05 20:21:02 -0300

    att api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://length-households-adaptive-silence.trycloudflare.com/api',
+  baseURL: 'https://pounds-msgid-jazz-inbox.trycloudflare.com/api',
   timeout: 10000,
 });
 
